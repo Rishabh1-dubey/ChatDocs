147de341e612983e0eef55e586f93eb462df44ef
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-02-23 13:56:28 +0530

    checking url for displaying the pdf

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -11,7 +11,7 @@ interface PageProps {
 }
 
 const Page = async ({ params }: PageProps) => {
-  const { fileid } = params;
+  const { fileid } =await params;
 
   const { getUser } = getKindeServerSession();
   const user = await getUser();
@@ -36,7 +36,7 @@ const Page = async ({ params }: PageProps) => {
 
         <div className="flex-1 xl:flex">
           <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-            <PdfRenderer/>
+            <PdfRenderer url={file.url}   />
           </div>
         </div>
 
