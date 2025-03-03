b39c39c50aee92988e284223de2d6e6c2e71f528
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-03-03 17:47:11 +0530

    ading force dynamically if user in null

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -2,6 +2,11 @@ import BillingForm from '@/components/BillingForm'
 import { getUserSubscriptionPlan } from '@/lib/razorpay'
 import React from 'react'
 
+
+
+
+export const dynamic = "force-dynamic";
+
 const page = async () => {
 
     const subscriptionPlan = await getUserSubscriptionPlan()
