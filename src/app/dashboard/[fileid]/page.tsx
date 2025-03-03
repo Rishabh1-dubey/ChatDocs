39d94eb2f36bde9479716a6aa1bcb9dc672c1255
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-03-03 13:31:51 +0530

    adding some changes in dashboard/fileid/pages

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -2,25 +2,22 @@ import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
 
-import PdfRenderer from  '@/components/PdfRenderer'
+import PdfRenderer from '@/components/PdfRenderer';
 import ChatWrapper from "@/components/chat/ChatWrapper";
+
 interface PageProps {
-  params: {
-    fileid: string;
-  };
+  params: Promise<{ fileid: string }>; // Define params as a Promise
 }
 
 const Page = async ({ params }: PageProps) => {
-  /* @next-codemod-ignore */
-  const { fileid } = await params;
+  const { fileid } =  await params; // No need to await params
 
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
   if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileid}`);
 
-  //make database call
-
+  // Make database call
   const file = await db.file.findFirst({
     where: {
       id: fileid,
@@ -31,19 +28,18 @@ const Page = async ({ params }: PageProps) => {
   if (!file) notFound();
 
   return (
-    <div className="flex justify-between  flex-col h-[calc(100vh-3.5rem)]">
+    <div className="flex justify-between flex-col h-[calc(100vh-3.5rem)]">
       <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
-        {/* left side */}
-
+        {/* Left side */}
         <div className="flex-1 xl:flex">
           <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-            <PdfRenderer url={file.url}   />
+            <PdfRenderer url={file.url} />
           </div>
         </div>
 
-        <div className="shrink-0 flex-[0.75] border-t border-gray-200  lg:w-96 lg:border-l lg:border-t-0">
-
-            <ChatWrapper fileId={file.id}/>
+        {/* Right side */}
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper fileId={file.id} />
         </div>
       </div>
     </div>
