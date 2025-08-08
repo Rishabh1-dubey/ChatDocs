cd7e2905718dc99a3981b9d35615a1982640bb29
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-08-08 14:14:49 +0530

    bug fix

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -1,77 +1,77 @@
-import { db } from '@/db'
-import { stripe } from '@/lib/stripe'
-import { headers } from 'next/headers'
-import type Stripe from 'stripe'
+// import { db } from '@/db'
+// import { stripe } from '@/lib/stripe'
+// import { headers } from 'next/headers'
+// import type Stripe from 'stripe'
 
-export async function POST(request: Request) {
-  const body = await request.text()
-  const signature = (await headers()).get('Stripe-Signature') ?? ''
+// export async function POST(request: Request) {
+//   const body = await request.text()
+//   const signature = (await headers()).get('Stripe-Signature') ?? ''
 
-  let event: Stripe.Event
+//   let event: Stripe.Event
 
-  try {
-    event = stripe.webhooks.constructEvent(
-      body,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET || ''
-    )
-  } catch (err) {
-    return new Response(
-      `Webhook Error: ${
-        err instanceof Error ? err.message : 'Unknown Error'
-      }`,
-      { status: 400 }
-    )
-  }
+//   try {
+//     event = stripe.webhooks.constructEvent(
+//       body,
+//       signature,
+//       process.env.STRIPE_WEBHOOK_SECRET || ''
+//     )
+//   } catch (err) {
+//     return new Response(
+//       `Webhook Error: ${
+//         err instanceof Error ? err.message : 'Unknown Error'
+//       }`,
+//       { status: 400 }
+//     )
+//   }
 
-  const session = event.data
-    .object as Stripe.Checkout.Session
+//   const session = event.data
+//     .object as Stripe.Checkout.Session
 
-  if (!session?.metadata?.userId) {
-    return new Response(null, {
-      status: 200,
-    }) 
-  }
+//   if (!session?.metadata?.userId) {
+//     return new Response(null, {
+//       status: 200,
+//     }) 
+//   }
 
 
 
-  if (event.type === 'checkout.session.completed') {
-    const subscription =
-    await stripe.subscriptions.retrieve(
-      session.subscription as string
-    ) 
+//   if (event.type === 'checkout.session.completed') {
+//     const subscription =
+//     await stripe.subscriptions.retrieve(
+//       session.subscription as string
+//     ) 
     
-    console.log("checking the subscription section in our databaser::::::::::",subscription);
-    await db.user.update({
-      where: {
-        id: session.metadata.userId,
-      },
+//     console.log("checking the subscription section in our databaser::::::::::",subscription);
+//     await db.user.update({
+//       where: {
+//         id: session.metadata.userId,
+//       },
     
-      data: {
-        stripeSubscriptionId: subscription.id,
-        stripeCustomerId: subscription.customer as string,
-        stripePriceId: subscription.items.data[0]?.price.id, 
-      },
-    }) 
-  }
+//       data: {
+//         stripeSubscriptionId: subscription.id,
+//         stripeCustomerId: subscription.customer as string,
+//         stripePriceId: subscription.items.data[0]?.price.id, 
+//       },
+//     }) 
+//   }
 
-  if (event.type === 'invoice.payment_succeeded') {
-    // Retrieve the subscription details from Stripe.
-    const subscription =
-      await stripe.subscriptions.retrieve(
-        session.subscription as string
-      ) as Stripe.Subscription
+//   if (event.type === 'invoice.payment_succeeded') {
+//     // Retrieve the subscription details from Stripe.
+//     const subscription =
+//       await stripe.subscriptions.retrieve(
+//         session.subscription as string
+//       ) as Stripe.Subscription
 
-    await db.user.update({
-      where: {
-        stripeSubscriptionId: subscription.id,
-      },
-      data: {
-        stripePriceId: subscription.items.data[0]?.price.id,
+//     await db.user.update({
+//       where: {
+//         stripeSubscriptionId: subscription.id,
+//       },
+//       data: {
+//         stripePriceId: subscription.items.data[0]?.price.id,
         
-      },
-    })
-  }
+//       },
+//     })
+//   }
 
-  return new Response(null, { status: 200 })
-}
+//   return new Response(null, { status: 200 })
+// }
