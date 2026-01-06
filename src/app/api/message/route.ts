a3ecdbd46e6bdfdb9e39ec31fcc7a8e5db4bcbb8
Rishabh1-dubey <rishabhdubey006@gmail.com>
2026-01-07 00:26:33 +0530

    route.ts fix it

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -10,6 +10,9 @@ export const POST = async (req: NextRequest) => {
 
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+  if (!user || !user.id) {
+    return new Response("Unauthorized", { status: 401 });
+  }
   const { id: userId } = user;
 
   if (!userId) return new Response("Unauthorized", { status: 401 });
